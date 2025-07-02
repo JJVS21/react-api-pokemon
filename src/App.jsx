65a133421c5280082e826cc8e3dd29cd626b75c0
jordan <jordan@gmail.com>
2025-07-02 17:09:52 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PokemonFetcher from './PokemonFetcher'; //Asegurate
+
+function App() {
+  return (
+    <>
+      <h1>¡Conoce a tus Pokémon!</h1>
+        <PokemonFetcher />
+    </>
+  )
+}
+
+export default App
